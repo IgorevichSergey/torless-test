2c4ea94e7d551a873858de356ad367012840b96c
Chelnokova Ruslana <gramazekamf@gmail.com>
2017-09-21 18:11:03 +0300

    image createng error redirect

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   host: 'https://tor-less.com/torless-admin/public/front_api_all_tags',
-  companyImage: 'http://tor-less.com/torless-admin/public/addCompanyImageAjax',
-  cafeteriaImage: 'http://tor-less.com/torless-admin/public/addCafeteriaImageAjax',
-  productImage: 'http://tor-less.com/torless-admin/public/addProductImageAjax'
+  companyImage: 'https://tor-less.com/torless-admin/public/addCompanyImageAjax',
+  cafeteriaImage: 'https://tor-less.com/torless-admin/public/addCafeteriaImageAjax',
+  productImage: 'https://tor-less.com/torless-admin/public/addProductImageAjax'
 };
