5b9841fff0fe4f3545cd9dadc5c97ac1e151c73a
Igorevich Sergey <gramazekamf@gmail.com>
2017-11-04 17:49:11 +0200

    in progress

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 
 export const environment = {
   production: true,
-  host: 'http://api.tor-less.com/front_api_all_tags',
-  companyImage: 'http://api.tor-less.com/addCompanyImageAjax',
-  cafeteriaImage: 'http://api.tor-less.com/addCafeteriaImageAjax',
-  productImage: 'http://api.tor-less.com/addProductImageAjax'
+  host: 'https://api.tor-less.com/front_api_all_tags',
+  companyImage: 'https://api.tor-less.com/addCompanyImageAjax',
+  cafeteriaImage: 'https://api.tor-less.com/addCafeteriaImageAjax',
+  productImage: 'https://api.tor-less.com/addProductImageAjax'
 };
